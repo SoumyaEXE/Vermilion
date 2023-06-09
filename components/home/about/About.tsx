f56fa46620202597403fe954d71be2e322d28897
Soumyadeep Dey <95275364+SoumyaEXE@users.noreply.github.com>
2023-06-09 14:16:26 +0530

    Update About.tsx

--- components/home/about/About.tsx
+++ components/home/about/About.tsx
@@ -11,7 +11,7 @@ export const About = () => {
         <div>
           <Reveal>
             <p className={`${styles.aboutText}`}>
-              <span>Hi</span> there! My name is Shiva Bhattacharjee, and I&apos;m a Computer Science student with a passion for creating and designing websites. I have two years of experience in web development, specializing in the MERN stack. I enjoy coding and strive to build modern and visually appealing websites that provide excellent user experiences. I am proficient in front-end technologies such as HTML, CSS, JavaScript, and React, as well as back-end technologies like Node.js and MongoDB. I am always eager to learn and explore new technologies and frameworks. If you&apos;re looking for a developer who can deliver high-quality websites with a creative touch, I&apos;m here to help. Let&apos;s collaborate and bring your vision to life!
+              <span>Hi</span> there! My name is Soumyadeep Dey, and I&apos;m a class 12th student with a passion for creating and designing websites. I am learning experience in web development, specializing to the best. I enjoy coding and strive to build modern and visually appealing websites that provide excellent user experiences. I am proficient in front-end technologies such as HTML, CSS, JavaScript, and React, as well as back-end am learning technologies like Node.js and MongoDB. I am always eager to learn and explore new technologies and frameworks. If you&apos;re looking for a developer who can deliver attractive websites with a creative touch, I&apos;m here to help. Let&apos;s collaborate and bring your vision to life!
             </p>
           </Reveal>
         </div>
