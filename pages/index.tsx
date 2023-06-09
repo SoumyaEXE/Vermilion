1e5b5f4b833a5488a20ee3b3932223a45b5de7bc
Soumyadeep Dey <95275364+SoumyaEXE@users.noreply.github.com>
2023-06-09 14:08:07 +0530

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ export default function home() {
   return (
     <>
       <Head>
-        <title> Soumya | Learning Web Development </title>
+        <title> Soumyadeep | Learning Web Development </title>
         <meta name="description" content=" I am a beginner web developer " />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/shivalogo.ico" />
